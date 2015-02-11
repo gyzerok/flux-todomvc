8ea730f54616665009771cea29f6988d098f9d83
Fedor Nezhivoy <gyzerok@gmail.com>
2015-02-11 16:05:14 +0600

    creating little implementation of Flux & React

---
+++ src/lib/Influx.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var React = require('react');
+var FBDispatcher = require('flux').Dispatcher;
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+
+var CHANGE_EVENT = 'influx:change';
+
+module.exports = {
+
+    createClass: function (opts) {
+        // TODO: do we need merge mixins here?
+        return React.createClass(opts);
+    },
+
+    createStore: function (opts) {
+
+        var Store = assign({}, EventEmitter, {
+            actions: {},
+
+            emitChange: function () {
+                this.emit(CHANGE_EVENT);
+            },
+
+            onChange: function (callback) {
+                this.on(CHANGE_EVENT, callback);
+            },
+
+            offChange: function (callback) {
+                this.removeListener(CHANGE_EVENT, callback);
+            }
+        }, opts);
+
+        return Store;
+    },
+
+    createDispatcher: function (opts) {
+        var Dispatcher = new FBDispatcher();
+        var subs = opts.subscribers();
+        subs.forEach(function (sub) {
+            if (!sub.actions) throw new Error('Incorrect subscriber');
+
+            Dispatcher.register(function (payload) {
+                var action = payload.action;
+
+                for (var actionType in subs.actions) {
+                    if (actionType !== action.actionType) return;
+                    sub.actions[actionType]().bind(this);
+                    sub.emitChange();
+                }
+            });
+        });
+    }
+};
